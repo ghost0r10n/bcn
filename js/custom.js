715efa87c7175a4e0bfc063665b1e4c11221e706
Fabio Capocasale <f.capocasale@be-tse.it>
2024-04-03 00:44:10 +0200

    fixes: finito tutte le sezioni + scroll fix

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,9 @@
+function toggleCollapse() {
+  const element = document.getElementById("navbarNav");
+  if (element.classList.contains('collapse')) {
+    element.classList.remove('collapse');
+  } else {
+    element.classList.add('collapse');
+  }
+}
 
